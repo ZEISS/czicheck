8179d4b87eb4452a8b727f5f270a3a86c22905c3
Juergen Bohl <juergen.bohl@zeiss.com>
2026-05-19 01:09:43 +0200

    linter

--- CZICheck/checkers/utils/planeenumerator.h
+++ CZICheck/checkers/utils/planeenumerator.h
@@ -47,7 +47,7 @@ private:
         kOrderOfDimensionsToIterate.size() ==
             (static_cast<std::size_t>(libCZI::DimensionIndex::MaxDim) -
              static_cast<std::size_t>(libCZI::DimensionIndex::MinDim) + 1U),
-        "kOrderOfDimensionsToIterate must contain exactly one entry per valid DimensionIndex.");
+            "kOrderOfDimensionsToIterate must contain exactly one entry per valid DimensionIndex.");
 
     const libCZI::CDimBounds& GetDimBounds() const
     {
